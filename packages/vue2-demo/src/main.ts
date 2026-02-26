e7dfa86c85e094daf5dc4fdf51f41a1417dbf695
MengFangui <meng010387@126.com>
2026-02-26 16:26:28 +0800

    fix: 修复 vue2-demo main.ts 导入路径
    - 修改 main.ts 中 @tdesign/td-chat 为 @jump-mp/td-chat

--- packages/vue2-demo/src/main.ts
+++ packages/vue2-demo/src/main.ts
@@ -1,6 +1,6 @@
 import Vue from 'vue';
 import TDesign from 'tdesign-vue';
-import TDesignChat from '@tdesign/td-chat';
+import TDesignChat from '@jump-mp/td-chat';
 import Demo from './demo.vue';
 import 'tdesign-vue/es/style/index.css';
 import './index.css';
